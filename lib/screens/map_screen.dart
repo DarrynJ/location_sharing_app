70ff1fc7a60fd4894c60e0684dfc2e32dd6e8c85
DarrynJ <djacobs900@gmail.com>
2022-10-21 09:27:25 +0200

    added location

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -9,18 +10,73 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  LatLng initialLocation = const LatLng(37.422131, -122.084801);
-// ToDo: add custom marker
+  LatLng? initialLocation = const LatLng(
+      -25.777337119077238, 28.25658729797763); // Agile Bridge offices
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initLocation().then((value) => {
+          if (value != null)
+            {
+              setState(() {
+                initialLocation = LatLng(value.latitude!, value.longitude!);
+              })
+            }
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
+    BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+
     return Scaffold(
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
-          target: initialLocation,
+          target: initialLocation!,
           zoom: 14,
         ),
-        // ToDO: add markers
+        markers: {
+          Marker(
+            markerId: const MarkerId("marker1"),
+            position: initialLocation!,
+            draggable: true,
+            onDragEnd: (value) {
+              // value is the new position
+            },
+            icon: markerIcon,
+          ),
+        },
       ),
     );
+  }
+
+  Future<LocationData?> _initLocation() async {
+    Location location = Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return null;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return null;
+      }
+    }
+
+    _locationData = await location.getLocation();
+
+    return _locationData;
   }
 }
