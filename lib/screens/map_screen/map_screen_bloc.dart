83d69a5ead7e5fea72e8a4decdeb3a302bd97861
DarrynJ <djacobs900@gmail.com>
2022-10-21 12:35:17 +0200

    cleaned up code and added navigation and bloc

---
+++ lib/screens/map_screen/map_screen_bloc.dart
@@ -0,0 +1,65 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'map_screen_event.dart';
+part 'map_screen_state.dart';
+
+class MapScreenBloc extends Bloc<MapScreenEvent, MapScreenState> {
+  MapScreenBloc() : super(MapScreenInitial()) {
+    on<MapScreenEvent>((event, emit) {});
+    on<GetMyCurrentLocation>(_getMyCurrentLocation);
+    on<ShareLocation>(_shareCurrentLocation);
+  }
+
+  _getMyCurrentLocation(
+      GetMyCurrentLocation event, Emitter<MapScreenState> emit) async {
+    try {
+      Location location = Location();
+
+      bool _serviceEnabled;
+      PermissionStatus _permissionGranted;
+      LocationData _locationData;
+
+      _serviceEnabled = await location.serviceEnabled();
+      if (!_serviceEnabled) {
+        _serviceEnabled = await location.requestService();
+        if (!_serviceEnabled) {
+          return null;
+        }
+      }
+
+      _permissionGranted = await location.hasPermission();
+      if (_permissionGranted == PermissionStatus.denied) {
+        _permissionGranted = await location.requestPermission();
+        if (_permissionGranted != PermissionStatus.granted) {
+          return null;
+        }
+      }
+
+      _locationData = await location.getLocation();
+
+      emit(CurrentLocation(
+          LatLng(_locationData.latitude!, _locationData.longitude!)));
+    } catch (e) {
+      print(e);
+      emit(MapScreenError(MapScreenErrors.GET_CURRENT_LOCATION_ERROR));
+    }
+  }
+
+  _shareCurrentLocation(ShareLocation event, Emitter<MapScreenState> emit) {
+    try {
+      emit(SharedCurrentLocation());
+    } catch (e) {
+      print(e);
+      emit(MapScreenError(MapScreenErrors.LOCATION_SHARING_ERROR));
+    }
+  }
+}
+
+enum MapScreenErrors {
+  DEFAULT,
+  GET_CURRENT_LOCATION_ERROR,
+  LOCATION_SHARING_ERROR,
+}
---
+++ lib/screens/map_screen/map_screen_event.dart
@@ -0,0 +1,12 @@
+part of 'map_screen_bloc.dart';
+
+@immutable
+abstract class MapScreenEvent {}
+
+class ShareLocation extends MapScreenEvent {
+  final LatLng location;
+
+  ShareLocation(this.location);
+}
+
+class GetMyCurrentLocation extends MapScreenEvent {}
